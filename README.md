50953da7e9947470f35a199a751c65b648ededf1
vvasileios <vtvasileiadis@gmail.com>
2023-03-25 13:53:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Face-Recognition, Back-End 
+
+Face-Recognition is an application that through Clarifai's Face-recognition-API, gives you the possibility to find the face on images.
+
+For the server of the web-app I used Node.js with Express.js.
+
+For the Database, I used PostgreSql and DBeaver which is a SQL client software application and a database management tool.
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+require('dotenv').config();
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  // connect to your own database here:
+  client: 'pg',
+  connection: {
+    host : process.env.IP_ADRESS,
+    port: process.env.DATABASE_PORT,
+    user : process.env.DATABASE_USER,
+    password : '',
+    database : process.env.DATABASE_NAME
+  }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
